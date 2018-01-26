6747666602d6d375b4378f9115f4a01966c79ce2
dotcloudbxrn <a2tonev@gmail.com>
2018-01-26 16:46:35 +0100

    help

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const port = process.env.PORT || 4040
 const env = process.env.NODE_ENV || 'development'
 const settings = require('./config/settings')[env]
 const app = require('./config/express')
+const cors = require('cors')
+
+app.use(cors())
 
 require('./config/db')(settings)
-require('./config/routes')(app)
+require('./routes')(app)
+
 app.listen(port)
 console.log(`Listening on port ${port}`)
